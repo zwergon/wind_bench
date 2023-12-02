cf8f5615262f460de7d04f7bba5e39add1d5b02f
zwergon <jef.lecomte@gmail.com>
2023-12-02 18:44:31 +0100

    remove S3Dataset from test

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,10 +1,9 @@
 import unittest
 import os
-import torch
 
 from wb.dataset.dataloader import NaiveDataLoader
 from wb.utils.time_utils import Timer
-from wb.dataset import S3WBDataset, FileWBDataset, NumpyWBDataset, AzureMLDataset
+from wb.dataset import FileWBDataset, NumpyWBDataset, AzureMLDataset
 
 
 class TestDataset(unittest.TestCase):
@@ -32,7 +31,38 @@ class TestDataset(unittest.TestCase):
 
         print(f"load batch size {self.batch_size} in {float(timer):.2f}s ({self.batch_size/float(timer):.2f} i/s)")
         
+    def test_az_ml_dataset(self):
+        print("AzureMLDataset")
+        dataset = AzureMLDataset("azureml://subscriptions/8a889cf2-7b3d-4003-824e-b503f56604b0/resourcegroups/rg-jef-ml/workspaces/ml-gpu/datastores/workspaceblobstore/paths/UI/2023-11-29_114659_UTC/wind_bench_1000_1028.parquet", indices=[0, 3])
+        
+        print(f"train dataset (size) {len(dataset)}")
 
+        dataloader = NaiveDataLoader(dataset, batch_size=self.batch_size)
+        with Timer() as timer:
+            for batch in dataloader:
+                X, y = batch
+                print(X.shape, y.shape)
+                break
+
+        print(f"load batch size {self.batch_size} in {float(timer):.2f}s ({self.batch_size/float(timer):.2f} i/s)")
+
+    def test_numpy_dataset(self):
+        print("NumpyWBDataset")
+        dataset = NumpyWBDataset(self.root_path, indices=[0, 3])
+        
+        print(f"train dataset (size) {len(dataset)}")
+
+        dataloader = NaiveDataLoader(dataset, batch_size=self.batch_size)
+        with Timer() as timer:
+            for batch in dataloader:
+                X, y = batch
+                print(X.shape, y.shape)
+                break
+
+        print(f"load batch size {self.batch_size} in {float(timer):.2f}s ({self.batch_size/float(timer):.2f} i/s)")
+
+
+    
     # def test_s3_dataset(self):
     #     print("S3WBDataset")
     #     dataset = S3WBDataset("wind_bench.parquet")
@@ -64,36 +94,6 @@ class TestDataset(unittest.TestCase):
     #     dataset.close()
 
     #    print(f"load batch size {self.batch_size} in {float(timer):.2f}s ({self.batch_size/float(timer):.2f} i/s)")
-
-    def test_az_ml_dataset(self):
-        print("AzureMLDataset")
-        dataset = AzureMLDataset("azureml://subscriptions/8a889cf2-7b3d-4003-824e-b503f56604b0/resourcegroups/rg-jef-ml/workspaces/ml-gpu/datastores/workspaceblobstore/paths/UI/2023-11-29_114659_UTC/wind_bench_1000_1028.parquet", indices=[0, 3])
-        
-        print(f"train dataset (size) {len(dataset)}")
-
-        dataloader = NaiveDataLoader(dataset, batch_size=self.batch_size)
-        with Timer() as timer:
-            for batch in dataloader:
-                X, y = batch
-                print(X.shape, y.shape)
-                break
-
-        print(f"load batch size {self.batch_size} in {float(timer):.2f}s ({self.batch_size/float(timer):.2f} i/s)")
-
-    def test_numpy_dataset(self):
-        print("NumpyWBDataset")
-        dataset = NumpyWBDataset(self.root_path, indices=[0, 3])
-        
-        print(f"train dataset (size) {len(dataset)}")
-
-        dataloader = NaiveDataLoader(dataset, batch_size=self.batch_size)
-        with Timer() as timer:
-            for batch in dataloader:
-                X, y = batch
-                print(X.shape, y.shape)
-                break
-
-        print(f"load batch size {self.batch_size} in {float(timer):.2f}s ({self.batch_size/float(timer):.2f} i/s)")
 
 
 if __name__ == "__main__":
